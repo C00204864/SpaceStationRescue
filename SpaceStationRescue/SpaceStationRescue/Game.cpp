dba343bee34c1af7ed0f302fad77ded593ce8089
C00204864 <C00204864@itcarlow.ie>
2019-01-07 10:25:41 +0000

    Base Game Skeleton

---
+++ SpaceStationRescue/SpaceStationRescue/Game.cpp
@@ -0,0 +1,72 @@
+#include "Game.h"
+
+Game::Game() :
+	m_window{ sf::VideoMode{ 1920, 1080, 32 }, "Space Station Rescue" },
+	m_exitGame{ false } // When true game will exit
+{
+
+}
+
+
+Game::~Game()
+{
+
+}
+
+
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	sf::Time timePerFrame = sf::seconds(1.f / 60.f); // 960 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // As many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // At least 60 fps
+			update(timePerFrame); // 60 fps
+		}
+		render(); // As many as possible
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+	while (m_window.pollEvent(event))
+	{
+		if (sf::Event::Closed == event.type) // window message
+		{
+			m_window.close();
+		}
+		if (sf::Event::KeyPressed == event.type) //user key press
+		{
+			if (sf::Keyboard::Escape == event.key.code)
+			{
+				m_exitGame = true;
+			}
+		}
+	}
+}
+
+void Game::update(sf::Time t_deltaTime)
+{
+	if (m_exitGame)
+	{
+		m_window.close();
+	}
+	else if (m_window.hasFocus()) // Ensure window is in focus before any action is taken
+	{
+		// Update...
+	}
+}
+
+void Game::render()
+{
+	m_window.clear(sf::Color::Black);
+	// Draw...
+	m_window.display();
+}
---
+++ SpaceStationRescue/SpaceStationRescue/main.cpp
@@ -0,0 +1,31 @@
+/// <summary>
+/// Space Station Rescue
+/// 
+/// By Liam Hickey (C00204864) and Michael Bridgette (C00205948)
+/// </summary>
+
+#ifdef _DEBUG
+#pragma comment(lib, "sfml-graphics-d.lib")
+#pragma comment(lib, "sfml-audio-d.lib")
+#pragma comment(lib, "sfml-system-d.lib")
+#pragma comment(lib, "sfml-window-d.lib")
+#pragma comment(lib, "sfml-network-d.lib")
+#else
+#pragma comment(lib, "sfml-graphics.lib")
+#pragma comment(lib, "sfml-audio.lib")
+#pragma comment(lib, "sfml-system.lib")
+#pragma comment(lib, "sfml-window.lib")
+#pragma comment(lib, "sfml-network.lib")
+#endif
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+
+#include "Game.h"
+
+int main()
+{
+	Game game;
+	game.run();
+
+	return 0;
+}
